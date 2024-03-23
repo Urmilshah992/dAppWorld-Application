0164607b8f1e3edbd8d7f48c886efeaa853c4d09
root <root@DESKTOP-PI8J1VA>
2024-03-23 13:55:04 +0530

    update ComareString

---
+++ src/CompareTwoStrings/README.md
@@ -0,0 +1,7 @@
+## Compare Strings
+
+In this problem you are tasked to compare the strings in solidity smart contract to check if the strings are same or different.
+
+### Output:
+
+compareStrings(string memory str1, string memory str2) returns(bool) : This function takes two strings as arguments and returns true if they are identical. If the strings are not identical, the function returns false.
---
+++ src/CompareTwoStrings/compareTwoStrings.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+contract StringComparisonContract {
+    function compareStrings(
+        string memory str1,
+        string memory str2
+    ) external pure returns (bool) {
+        if (bytes(str1).length != bytes(str2).length) {
+            return false;
+        }
+        return
+            keccak256(abi.encodePacked(str1)) ==
+            keccak256(abi.encodePacked(str2));
+    }
+}
